6fd507bbde8ceff0bad3d043a03572ebbe220392
Igor Mota <igormota17@outlook.com>
2022-12-13 18:27:36 -0300

    Payment, one-to-one association

--- src/main/java/com/coursejpa/course/config/TestConfig.java
+++ src/main/java/com/coursejpa/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.coursejpa.course.entities.Category;
 import com.coursejpa.course.entities.Order;
 import com.coursejpa.course.entities.OrderItem;
+import com.coursejpa.course.entities.Payment;
 import com.coursejpa.course.entities.Product;
 import com.coursejpa.course.entities.User;
 import com.coursejpa.course.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		itemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
