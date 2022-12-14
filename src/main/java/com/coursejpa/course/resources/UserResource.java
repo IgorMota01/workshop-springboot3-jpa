773f491d635c4c0829ce05cbef5a09266a693615
unknown <igormota17@outlook.com>
2022-12-14 09:56:26 -0300

    User insert

--- src/main/java/com/coursejpa/course/resources/UserResource.java
+++ src/main/java/com/coursejpa/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.coursejpa.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.coursejpa.course.entities.User;
 import com.coursejpa.course.services.UserService;
@@ -30,5 +34,13 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findByid(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		//metodo para retornar status 201 (PADRAO)
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
