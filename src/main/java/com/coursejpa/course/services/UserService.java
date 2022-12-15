ab678cb55797febe5ecf152d4891d4444324aea4
unknown <igormota17@outlook.com>
2022-12-15 09:33:35 -0300

    Exception handling - delete

--- src/main/java/com/coursejpa/course/services/UserService.java
+++ src/main/java/com/coursejpa/course/services/UserService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.coursejpa.course.entities.User;
 import com.coursejpa.course.repositories.UserRepository;
+import com.coursejpa.course.services.exceptions.DatabaseException;
 import com.coursejpa.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -34,7 +36,9 @@ public class UserService {
 		try {
 		repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e){
-			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
 		}
 	}
 	
